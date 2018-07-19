0fadd0457a02c29e0301f38010480e133f3497f4
Unknown <matej.horvat@gmail.com>
2018-07-19 11:04:38 +0200

    Improved comments and exception handling

--- stats.R
+++ stats.R
@@ -9,13 +9,15 @@ if (!exists("db") || db$info()$server$ok != 1) {
     rm(db)
   url <- "mongodb://"
   if (nchar(Sys.getenv("MONGO_PASS")) > 0) 
-    url <- paste0(url, Sys.getenv("MONGO_PASS"), "@")
-  url <- paste0(url, "localhost")
+    url <- paste0(url, Sys.getenv("MONGO_PASS"), "@localhost:27017")
+  else
+    url <- paste0(url, "localhost:27017")
   db <- mongo("StellarVanityPool", "mydb", url)
 }
 
 # basic stats
-cat("Total number of generated addresses:", db$count("{}"), "\n")
+tot <- db$count("{}")
+cat("Total number of generated addresses:", tot, "\n")
 
 
 # get counts
@@ -25,16 +27,17 @@ gens <- sapply(lens, function(i) {
 })
 
 # plot results
-plot(lens, gens, log = "y", type = "b", ylim = c(1, 100000000), xlab = "Length", ylab = "Count")
+plot(lens, gens, log = "y", type = "b", ylim = c(1, 1e8), xlab = "Length", ylab = "Count")
 text(lens, gens, labels = gens, adj = c(-0.25, -0.5))
 abline(h = 10^(0:8), lwd = 0.5, lty = 1, col = "gray")
 abline(v = lens, lwd = 0.5, lty = 1, col = "gray")
-title(main = "Generated Stellar Vanity Addresses")
+title(main = paste0("Generated Stellar Vanity Addresses (", tot, " in total)"))
 box()
 
 # print interesting suffixes
 cat("Interesting addresses:\n")
-print(sort(db$find('{"$expr": { "$gt": [ { "$strLenCP": "$suffix" }, 6 ] }}')$suffix))
+int <-unique(sort(db$find('{"$expr": { "$gt": [ { "$strLenCP": "$suffix" }, 6 ] }}')$suffix))
+print(int)
 
 # close connection
 #rm(db)
--- vgen.py
+++ vgen.py
@@ -2,15 +2,20 @@
 from stellar_base.keypair import Keypair
 from pybloomfilter import BloomFilter
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, OperationFailure
+from collections import OrderedDict
 import csv
 import time
+import os
 import sys
 
 
-nBench = 1000
+# how frequently to output performance statistics
+NBENCH = 1000
 
-# global variables (list of keywords and associated Bloom filter)
-bf = BloomFilter(1e6, 1e-4)
+
+# global variables
+bf = BloomFilter(1e6, 1e-3)
 keywords = []
 maxlen = 0
 
@@ -18,30 +23,27 @@ maxlen = 0
 # load the list of interesting keywords (suffixes, prefixes)
 def load_keywords(filename):
 
-    global keywords
-    global maxlen
+    global keywords, maxlen
 
     ifile = open(filename, 'rU')
     reader = csv.reader(ifile, delimiter=',')
 
     for row in reader:
-        keyword = row[0].encode('utf-8')
-        bf.add(keyword)
-        keywords.append(keyword)
-
-    ifile.close()
+        if len(row[0]) > 3 and len(row[0]) <= 10:
+            keyword = row[0].encode('utf-8')
+            bf.add(keyword)
+            keywords.append(keyword)
 
     maxlen = len(keywords[0])
+    ifile.close()
 
     return None
 
 
-# find first matching suffix
+# find longest matching suffix using a Bloom filter
 def first_match(address):
 
-    # check if we get a hit in the Bloom filter (longest to shortest)
     for i in range(maxlen, -1, -1):
-        # we have a potential hit...
         if address[-i:] in bf:
             if address[-i:] in keywords:
                 return address[-i:]
@@ -53,9 +55,9 @@ def first_match(address):
 # simple exponential average
 class exp_avg():
     r0 = None
-    def smooth(self, r, alpha = 0.15):
+    def smooth(self, r, prec = 1, alpha = 0.05):
         self.r0 = r if self.r0 == None else  alpha * r + (1 - alpha) * self.r0
-        return self.r0
+        return round(self.r0, prec)
 
 
 # main function
@@ -63,42 +65,60 @@ if __name__ == '__main__':
 
     # load interesting keywords
     load_keywords('names/NAMES.txt')
+    print('Loaded {} keywords.'.format(len(keywords)))
 
-    # connect to MongoDB
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.mydb
-    col = db.StellarVanityPool
-    print('Existing number of vanity addresses: {}'.format(col.count('{}')))
+    # connect to MongoDB (also check for failures)
+    cred = os.getenv("MONGO_PASS")
+    client = MongoClient('mongodb://' + ('' if cred is None else cred + '@') + 'localhost:27017/',
+                         document_class=OrderedDict)
+    try:
+        client.admin.command('ismaster')
+    except ConnectionFailure:
+        print("MongoDB server not available!")
+        sys.exit(-1)
+    col = client.mydb.StellarVanityPool
+    try:
+        col.find_one({})
+    except OperationFailure:
+        print("Wrong MongoDB credentials provided!")
+        sys.exit(-1)
 
-    print('Generating new ones...')
     # generate!
+    print('Existing number of vanity addresses: {}'.format(col.count('{}')))
+    print('Generating new ones...')
     i = hits = oldhits = 0
-    e1 = exp_avg()
-    e2 = exp_avg()
+    eavg = { "total": exp_avg(), "hit": exp_avg() }
     oldTime = time.perf_counter()
+
     while True:
 
         try:
 
+            # generate and check for hit
             kp = Keypair.random()
             address = kp.address()
-
             hit = first_match(address)
+
+            # insert hit into database
             if hit is not None:
                 hits = hits + 1
-                col.insert_one({"suffix": hit.decode(), "public": address.decode(), "secret": kp.seed().decode()})
+                col.insert_one(OrderedDict([("suffix", hit.decode()),
+                                            ("public", address.decode()),
+                                            ("secret", kp.seed().decode())]))
 
+            # display performance stats
             i = i + 1
-            if i % nBench == 0:
+            if i % NBENCH == 0:
                 newTime = time.perf_counter()
                 dt = newTime - oldTime
-                print('Generation rate:', round(e1.smooth(1.0 * nBench / dt), 1), 'addr/sec\t',
-                      'Hit rate: ',  round(e2.smooth((hits - oldhits)/dt), 1), 'addr/sec')
+                print('Generation rate:', eavg["total"].smooth(1.0 * NBENCH / dt), 'addr/sec\t',
+                      'Hit rate: ',  eavg["hit"].smooth((hits - oldhits) / dt), 'addr/sec')
                 oldTime = newTime
                 oldhits = hits
 
+        # exit nicely if interrupted
         except KeyboardInterrupt:
 
             client.close()
             print('\nBye.')
-            sys.exit()
+            sys.exit(0)
